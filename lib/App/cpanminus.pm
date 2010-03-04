0a3e7fe1276cb5c3050018f5a4b433b250e00660
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-04 11:33:52 -0800

    Checking in changes prior to tagging of version 0.9916.  Changelog diff is:
    diff --git a/Changes b/Changes
    index af9d0c5..491ecfd 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     See http://github.com/miyagawa/cpanminus/ for the latest development.
    
    +0.9916 Thu Mar  4 11:33:16 PST 2010
    +   [Developer Fixes]
    +      - Use 'distfile' as cpanmetadb key rather than 'dist' (rjbs, xdg)
    +
     0.9915 Thu Mar  4 01:56:06 PST 2010
        [Developer Fixes]
           - Fixed circular references (hopefully)

--- lib/App/cpanminus.pm
+++ lib/App/cpanminus.pm
@@ -1,5 +1,5 @@
 package App::cpanminus;
-our $VERSION = "0.9915";
+our $VERSION = "0.9916";
 
 =head1 NAME
 
--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -11,7 +11,7 @@ use Getopt::Long ();
 use constant WIN32 => $^O eq 'MSWin32';
 use constant PLUGIN_API_VERSION => 0.1;
 
-our $VERSION = "0.9915";
+our $VERSION = "0.9916";
 $VERSION = eval $VERSION;
 
 my $quote = WIN32 ? q/"/ : q/'/;
