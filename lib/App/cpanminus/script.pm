e3027db9787c5e1bd08c7614bebb0204545bf98f
HIROSE Masaaki <hirose31@gmail.com>
2010-10-14 19:03:46 +0900

    doc update

--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -252,6 +252,8 @@ Examples:
   cpanm MIYAGAWA/Plack-0.99_05.tar.gz                       # full distribution path
   cpanm http://example.org/LDS/CGI.pm-3.20.tar.gz           # install from URL
   cpanm ~/dists/MyCompany-Enterprise-1.00.tar.gz            # install from a local file
+  cpanm http://gtihub.com/miyagawa/Plack.git                # install from github
+  cpanm git://gtihub.com/miyagawa/Plack.git                 # ditto
   cpanm --interactive Task::Kensho                          # Configure interactively
   cpanm .                                                   # install from local directory
   cpanm --installdeps .                                     # install all the deps for the current directory
