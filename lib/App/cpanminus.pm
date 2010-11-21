7e082b4367d04443f08d5e85857fbf83cb232057
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-11-21 01:41:57 -0800

    overhaul the document

--- lib/App/cpanminus.pm
+++ lib/App/cpanminus.pm
@@ -26,7 +26,7 @@ search for cpanminus and use the appropriate command to install. This makes it
 easy to install C<cpanm> to your system without thinking about where to
 install, and later upgrade.
 
-You can also use the latest cpanminus to install cpanminus itself (aka bootstrap):
+You can also use the latest cpanminus to install cpanminus itself:
 
     curl -L http://cpanmin.us | perl - --sudo App::cpanminus
 
@@ -61,19 +61,13 @@ perl 5.8 or later.
 
 C compiler, if you want to build XS modules.
 
-=back
+=item *
 
-And optionally:
-
-=over 4
+make
 
 =item *
 
-make, if you want to reliably install MakeMaker based modules
-
-=item *
-
-Module::Build (core in 5.10) to install Build.PL based modules
+Module::Build (core in 5.10)
 
 =back
 
@@ -100,64 +94,6 @@ their feet, setting up the CPAN toolchain often feels like yak shaving,
 especially when all they want to do is just install some modules and start
 writing code.
 
-In particular, here are the few issues I've observed:
-
-=over
-
-=item *
-
-They ask too many questions and do not provide enough sane defaults. A normal
-user doesn't (and shouldn't have to) know what's the right answer for the
-question C<Parameters for the 'perl Build.PL' command? []>
-
-=item *
-
-They provide very noisy output by default.
-
-=item *
-
-They fetch and rebuild their indexes almost every day, and this takes time.
-
-=item *
-
-... and they hog 200MB of memory and thrashes/OOMs on my 256MB VPS
-
-=back
-
-cpanminus is designed to be very quiet (but logs all output to
-C<~/.cpanm/build.log>) and to pick the sanest possible defaults without asking
-any questions -- to I<just work>.
-
-Note that most of these problems with existing tools are rare, or are just
-overstated.  They might already be fixed, or can be configured to work nicer.
-For instance, the latest CPAN.pm dev release has a much better first time
-configuration experience than ever before.
-
-I know there's a reason for them to have many options and questions, because
-they're meant to work everywhere for everybody.
-
-Of course I should have contributed back to CPAN/CPANPLUS
-instead of writing a new client, but CPAN.pm is nearly impossible
-(for anyone other than andk or xdg) to maintain (that's why CPANPLUS
-was born, right?) and CPANPLUS is a huge beast for me to start working
-on.
-
-=head2 Are you on drugs?
-
-Yeah, I think my brain has been damaged since I looked at PyPI,
-gemcutter, pip and rip. They're quite nice and I really wanted
-something as nice for CPAN which I love.
-
-=head2 How does this thing work?
-
-Imagine you don't have CPAN or CPANPLUS. You search for a module on the CPAN
-search site, download a tarball, unpack it and then run C<perl Makefile.PL> (or
-C<perl Build.PL>). If the module has dependencies you probably have to resolve
-those dependencies by hand before doing so. Then you run the unit tests and
-C<make install> (or C<./Build install>).
-
-cpanminus automates that.
-
 =head2 Zero-conf? How does this module get/parse/update the CPAN index?
 
 It queries the CPAN Meta DB site running on Google AppEngine at
@@ -181,58 +117,6 @@ the permission to install modules to the sitelib directory.  If neither, it
 automatically sets up local::lib compatible installation path in a C<perl5>
 directory under your home directory. To avoid this, run the script as the root
 user, with C<--sudo> option or with C<--local-lib> option.
-
-This L<local::lib> automatic integration is still considered alpha and
-in the work -- more bootstrapping is under development. Stay tuned.
-
-=head2 Does this really work?
-
-I tested installing MojoMojo, Task::Kensho, KiokuDB, Catalyst, Jifty and Plack
-using cpanminus and the installations including dependencies were mostly
-successful. More than I<half of CPAN> behaves really nicely and appears to
-work.
-
-However, there might be some distributions that will miserably fail, because of
-nasty edge cases. Here are some examples:
-
-=over 4
-
-=item *
-
-Packages uploaded to PAUSE in 90s which don't live under the standard
-C<authors/id/A/AA> directory hierarchy.
-
-=item *
-
-Distributions with a C<Makefile.PL> or C<Build.PL> that asks you questions
-without using C<prompt> function. However cpanminus has a mechanism to kill
-those questions with a timeout, and you can always say C<--interactive> to make
-the configuration interactive.
-
-=item *
-
-Distributions that do not shipped with C<META.yml> file but do require
-some specific version of toolchain for configuration.
-
-=item *
-
-Distributions that have a C<META.yml> file that is encoded in YAML 1.1 format
-using L<YAML::XS>. This will be eventually solved once we move to C<META.json>.
-
-=back
-
-cpanminus intends to work for 99.9% of modules on CPAN for 99.9% of people. It
-may not be perfect, but it should just work in most cases.
-
-If this tool doesn't work for your very rare environment, then I'm sorry, but
-you should use CPAN or CPANPLUS, or build and install modules manually.
-
-=head2 That sounds fantastic. Should I switch to this from CPAN(PLUS)?
-
-If you have CPAN or CPANPLUS working then you may want to keep using
-CPAN or CPANPLUS in the longer term, but I hope this can be a
-quite handy alternative to them for people in other situations. And
-apparently, many people love (at least the idea of) this software :)
 
 =head1 COPYRIGHT
 
