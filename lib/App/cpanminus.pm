a6ed0199f3785289ddf6bd3958fcc4ecb5217cce
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-01-18 14:11:36 -0800

    Checking in changes prior to tagging of version 1.19_02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 18254a9..d0ae86f 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     See http://github.com/miyagawa/cpanminus/ for the latest development.
    
    +1.19_02 Tue Jan 18 14:11:23 PST 2011
    +   - Module::Metadata now requires version.pm 0.87. This makes cpanm unable to boostrap when version.pm is
    +     older than that. For now, include the pure perl version/vpp.pm and version.pm in the fatlib and then
    +     fatpacked in cpanm executable #78
    +
     1.19_01 Tue Jan 18 12:48:47 PST 2011
        - Upgraded bundled modules, notably Module::Metadata and local::lib
        - Switched the pure perl fallback to HTTP::Tiny

--- lib/App/cpanminus.pm
+++ lib/App/cpanminus.pm
@@ -1,5 +1,5 @@
 package App::cpanminus;
-our $VERSION = "1.19_01";
+our $VERSION = "1.19_02";
 
 =head1 NAME
 
--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -12,7 +12,7 @@ use Parse::CPAN::Meta;
 use constant WIN32 => $^O eq 'MSWin32';
 use constant SUNOS => $^O eq 'solaris';
 
-our $VERSION = "1.19_01";
+our $VERSION = "1.19_02";
 $VERSION = eval $VERSION;
 
 my $quote = WIN32 ? q/"/ : q/'/;
