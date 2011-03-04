2eef600367016aff5350405cc33297391f738d0f
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-03-04 01:40:49 -0800

    Checking in changes prior to tagging of version 1.30_05.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 4b6f49a..ce269ed 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     See http://github.com/miyagawa/cpanminus/ for the latest development.
    
    +1.30_05  Fri Mar  4 01:36:28 PST 2011
    +   - Fixed the way it reduces @INC in the tests when -L is in use, so the tests
    +     that call $^X with -I can keep those library paths. i.e. Test::More
    +
     1.30_04  Fri Mar  4 00:46:24 PST 2011
        - Support minor case where blib/* is manually added via -I (e.g. FCGI.pm)

--- lib/App/cpanminus.pm
+++ lib/App/cpanminus.pm
@@ -1,5 +1,5 @@
 package App::cpanminus;
-our $VERSION = "1.30_04";
+our $VERSION = "1.30_05";
 
 =head1 NAME
 
--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -12,7 +12,7 @@ use Parse::CPAN::Meta;
 use constant WIN32 => $^O eq 'MSWin32';
 use constant SUNOS => $^O eq 'solaris';
 
-our $VERSION = "1.30_04";
+our $VERSION = "1.30_05";
 
 my $quote = WIN32 ? q/"/ : q/'/;
 
