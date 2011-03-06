74c07c3ad50ebf6a97962cf8249d9201bc11a483
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-03-06 01:36:43 -0800

    Checking in changes prior to tagging of version 1.30_11.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 309b251..4a7a44a 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,11 @@
     See http://github.com/miyagawa/cpanminus/ for the latest development.
    
    +1.30_11  Sun Mar  6 01:34:59 PST 2011
    +   - Fix SIGNATURE testing for Module::Build 0.38 or later
    +   - Implemented EXPERIMENTAL --scandeps command and --format option
    +   - Implemented EXPERIMENTAL --save-tarballs option
    +   - Show (undef) in the version output instead of ()
    +
     1.30_10  Sat Mar  5 14:55:16 PST 2011
        - Changed how it special cases Module::Build load path in -L mode. It's much more
          reliable and support 0.38 with the new CPAN::Meta dependencies

--- lib/App/cpanminus.pm
+++ lib/App/cpanminus.pm
@@ -1,5 +1,5 @@
 package App::cpanminus;
-our $VERSION = "1.30_10";
+our $VERSION = "1.30_11";
 
 =head1 NAME
 
--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -12,7 +12,7 @@ use Parse::CPAN::Meta;
 use constant WIN32 => $^O eq 'MSWin32';
 use constant SUNOS => $^O eq 'solaris';
 
-our $VERSION = "1.30_10";
+our $VERSION = "1.30_11";
 
 my $quote = WIN32 ? q/"/ : q/'/;
 
