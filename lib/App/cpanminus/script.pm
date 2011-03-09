41de7eaafbf6f0966990eeb677040c12e5ccf697
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-03-08 17:56:57 -0800

    Checking in changes prior to tagging of version 1.4002.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index fd83e79..8edf895 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     See http://github.com/miyagawa/cpanminus/ for the latest development.
    
    +1.4002  Tue Mar  8 17:47:51 PST 2011
    +      - Display configure errors in --scandeps so that configure_requires can be
    +        manually satisfied (suggested by marcus)
    +
     1.4001  Tue Mar  8 10:10:28 PST 2011
           - Fixed a workaround code for META.yml being JSON (reported by leedo)

--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -14,7 +14,7 @@ use Symbol ();
 use constant WIN32 => $^O eq 'MSWin32';
 use constant SUNOS => $^O eq 'solaris';
 
-our $VERSION = "1.4001";
+our $VERSION = "1.4002";
 
 my $quote = WIN32 ? q/"/ : q/'/;
 
