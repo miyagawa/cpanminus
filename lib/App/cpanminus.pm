a93c7de3364c17c3d72881bc1a302f66ceff4752
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-06-12 18:32:32 -0700

    Checking in changes prior to tagging of version 1.5014.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index bf6a4b7..fa7e89c 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,11 @@
     See http://github.com/miyagawa/cpanminus/ for the latest development.
    
    +1.5014 Tue Jun 12 18:27:02 PDT 2012
    +   [Improvements]
    +      - Make sure 'f' flag becomes the last for some tar versions (mst, aaronsw)
    +      - Fixed warnings on perl 5.17+ (rjbs)
    +      - Fix local::lib error message (berekuk)
    +
     1.5013 Sat May 12 06:15:44 EEST 2012
        [Bug Fixes]
           - Fixed --cascade-search to seach for missing modules, which was broken by #150

--- lib/App/cpanminus.pm
+++ lib/App/cpanminus.pm
@@ -1,5 +1,5 @@
 package App::cpanminus;
-our $VERSION = "1.5013";
+our $VERSION = "1.5014";
 
 =head1 NAME
 
--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -14,7 +14,7 @@ use Symbol ();
 use constant WIN32 => $^O eq 'MSWin32';
 use constant SUNOS => $^O eq 'solaris';
 
-our $VERSION = "1.5013";
+our $VERSION = "1.5014";
 
 my $quote = WIN32 ? q/"/ : q/'/;
 
