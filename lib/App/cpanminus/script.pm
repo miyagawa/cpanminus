ce5b9ff891deacae51b98c733273cbbf1c6c8c7d
ikasam_a <masaki.nakagawa@gmail.com>
2012-07-13 21:27:54 +0900

    support git:// and *.git

--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -7,6 +7,7 @@ use File::Find ();
 use File::Path ();
 use File::Spec ();
 use File::Copy ();
+use File::Temp ();
 use Getopt::Long ();
 use Parse::CPAN::Meta;
 use Symbol ();
@@ -1052,6 +1053,11 @@ sub resolve_name {
         };
     }
 
+    # Git
+    if ($module =~ /(^git:|\.git$)/) {
+        return $self->git_uri($module);
+    }
+
     # cpan URI
     if ($module =~ s!^cpan:///distfile/!!) {
         return $self->cpan_dist($module);
@@ -1100,6 +1106,21 @@ sub cpan_dist {
         $d->properties,
         source  => 'cpan',
         uris    => $url,
+    };
+}
+
+sub git_uri {
+    my ($self, $uri) = @_;
+
+    my $dh  = File::Temp->newdir(CLEANUP => 1);
+    my $dir = Cwd::abs_path($dh->dirname);
+
+    system 'git', 'clone', $uri, $dir;
+
+    return {
+        source => 'local',
+        dir => $dir,
+        _dh => $dh,
     };
 }
 
