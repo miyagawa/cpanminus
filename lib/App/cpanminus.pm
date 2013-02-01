4f8eacd83e76c14b8e60325c0edc20e448bb7e80
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-01-31 19:11:31 -0800

    Checking in changes prior to tagging of version 1.59_02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b2ba325..081828b 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,12 @@
     See http://github.com/miyagawa/cpanminus/ for the latest development.
    
    +1.59_02 Thu Jan 31 19:09:43 PST 2013
    +   [New Features]
    +      - Added experimental @ shortcut to mean exact version e.g. cpanm DBI@1.20
    +      - Support version range with "~", such as cpanm DBI~">= 1.0, < 2.0"
    +      - Enable metacpan release search when exact version is given (and when mirror-only is not specified)
    +        i.e. cpanm DBI@1.100 will fetch the exact release based on metacpan
    +
     1.59_01 Thu Jan 31 09:54:50 PST 2013
        [New Features]
           - Added a proper support for version range in MYMETA files using CPAN::Meta::Requirements

--- lib/App/cpanminus.pm
+++ lib/App/cpanminus.pm
@@ -1,5 +1,5 @@
 package App::cpanminus;
-our $VERSION = "1.59_01";
+our $VERSION = "1.59_02";
 
 =head1 NAME
 
--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -14,7 +14,7 @@ use Symbol ();
 use constant WIN32 => $^O eq 'MSWin32';
 use constant SUNOS => $^O eq 'solaris';
 
-our $VERSION = "1.59_01";
+our $VERSION = "1.59_02";
 
 my $quote = WIN32 ? q/"/ : q/'/;
 
