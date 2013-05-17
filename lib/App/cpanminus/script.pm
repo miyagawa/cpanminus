3ca89c0790e1e5820f65d61078254cd4d6d1a6f6
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-05-17 10:14:35 -0700

    Prefer install path from INSTALL_BASE when local::lib is setup

--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -221,10 +221,11 @@ sub parse_options {
 }
 
 sub check_upgrade {
+    my $install_path = ($ENV{PERL_MM_OPT} =~ /^INSTALL_BASE=(\S+)/)[0] || $Config{installsitebin};
     if ($0 eq '-') {
         # run from curl, that's fine
         return;
-    } elsif ($0 !~ /^$Config{installsitebin}/) {
+    } elsif ($0 !~ /^$install_path/) {
         if ($0 =~ m!perlbrew/bin!) {
             die <<DIE;
 It appears your cpanm executable was installed via `perlbrew install-cpanm`.
