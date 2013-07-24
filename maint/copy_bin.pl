a46d8712c80bd97c74047e1547e90ed4009f974d
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-07-24 11:41:45 -0700

    stop using sharedir. Copy cpanm into App/cpanminus/fatscript.pm and ship it to CPAN

--- maint/copy_bin.pl
+++ maint/copy_bin.pl
@@ -1,11 +1,17 @@
-# perl Makefile.PL (from git repo) copies 'cpanm' -> '{bin,share}/cpanm'
+# perl Makefile.PL (from git repo) copies 'cpanm' -> 'bin/cpanm'
+
+my $header = {
+    "bin/cpanm" => "#!perl", # so MakeMaker can fix it
+    "fatpacked/App/cpanminus/fatscript.pm" => "package App::cpanminus::fatscript;",
+};
+
 if (-e 'cpanm') {
-    for my $dir ("bin", "share") {
-        print STDERR "Generating $dir/cpanm from cpanm\n";
-        open my $in,  "<cpanm"     or die $!;
-        open my $out, ">$dir/cpanm" or die $!;
+    for my $file ("bin/cpanm", "fatpacked/App/cpanminus/fatscript.pm") {
+        print STDERR "Generating $file from cpanm\n";
+        open my $in,  "<cpanm" or die $!;
+        open my $out, ">$file" or die $!;
         while (<$in>) {
-            s|^#!/usr/bin/env perl|#!perl|; # so MakeMaker can fix it
+            s|^#!/usr/bin/env perl|$header->{$file}|;
             print $out $_;
         }
     }
