1481e5fcb23104163055f9776634f1e41e90ce19
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2014-08-08 14:36:29 -0700

    return true when skipping archive verification. Fix #402

--- lib/App/cpanminus/script.pm
+++ lib/App/cpanminus/script.pm
@@ -1660,6 +1660,7 @@ sub verify_archive {
 
     unless ($dist->{cpanid}) {
         $self->chat("Archive '$file' does not seem to be from PAUSE. Skip verification.\n");
+        return 1;
     }
 
     (my $mirror = $uri) =~ s!/authors/id.*$!!;
