b9d847db73054aa0506c35ab604b34ad91316052
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2014-09-17 16:33:43 +0900

    Make fatpacked version.pm the same as core version.pm, by removing the block as suggested.
       !!!!Delete this next block completely when adding to Perl core!!!!

--- fatlib/version.pm
+++ fatlib/version.pm
@@ -9,45 +9,6 @@ use vars qw(@ISA $VERSION $CLASS $STRICT $LAX *declare *qv);
 $VERSION = 0.9909;
 $CLASS = 'version';
 
-# !!!!Delete this next block completely when adding to Perl core!!!!
-{
-    local $SIG{'__DIE__'};
-    eval "use version::vxs $VERSION";
-    if ( $@ ) { # don't have the XS version installed
-	eval "use version::vpp $VERSION"; # don't tempt fate
-	die "$@" if ( $@ );
-	push @ISA, "version::vpp";
-	local $^W;
-	*version::qv = \&version::vpp::qv;
-	*version::declare = \&version::vpp::declare;
-	*version::_VERSION = \&version::vpp::_VERSION;
-	*version::vcmp = \&version::vpp::vcmp;
-	*version::new = \&version::vpp::new;
-	if ($] >= 5.009000) {
-	    no strict 'refs';
-	    *version::stringify = \&version::vpp::stringify;
-	    *{'version::(""'} = \&version::vpp::stringify;
-	    *{'version::(<=>'} = \&version::vpp::vcmp;
-	    *version::parse = \&version::vpp::parse;
-	}
-    }
-    else { # use XS module
-	push @ISA, "version::vxs";
-	local $^W;
-	*version::declare = \&version::vxs::declare;
-	*version::qv = \&version::vxs::qv;
-	*version::_VERSION = \&version::vxs::_VERSION;
-	*version::vcmp = \&version::vxs::VCMP;
-	*version::new = \&version::vxs::new;
-	if ($] >= 5.009000) {
-	    no strict 'refs';
-	    *version::stringify = \&version::vxs::stringify;
-	    *{'version::(""'} = \&version::vxs::stringify;
-	    *{'version::(<=>'} = \&version::vxs::VCMP;
-	    *version::parse = \&version::vxs::parse;
-	}
-    }
-}
 
 # avoid using Exporter
 require version::regex;
