30438689735f4ac5fcc045dd8e98329430442f8c
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2018-04-20 12:58:47 +0200

    bump HTTP::Tinyish

--- fatlib/HTTP/Tinyish.pm
+++ fatlib/HTTP/Tinyish.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Carp ();
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 
 our $PreferredBackend; # for tests
 our @Backends = map "HTTP::Tinyish::$_", qw( LWP HTTPTiny Curl Wget );
