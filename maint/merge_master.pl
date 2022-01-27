0fba98148a81daf080caa7272fe227d6e3b4308f
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2022-01-26 18:51:44 -0800

    don't merge from devel; use maint branch

--- maint/merge_master.pl
+++ maint/merge_master.pl
@@ -5,7 +5,7 @@ use CPAN::Meta;
 my $meta = CPAN::Meta->load_file('META.json');
 if ($meta->release_status eq 'stable') {
     system 'git', 'checkout', 'master';
-    system 'git', 'merge', '--ff-only', 'devel';
+    system 'git', 'merge', '--ff-only', 'maint';
     system 'git', 'push';
     system 'git', 'checkout', '-';
 } else {
